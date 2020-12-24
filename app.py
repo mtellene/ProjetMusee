a86a6862881d95ff5c7d6ae403358af25632810b
Maxime TELLENE <maxime.tellene@alumni.univ-avignon.fr>
2020-12-24 16:04:39 +0100

    changement de 2 salles, recuperation de la salle de chaque oeuvre ok

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from init import init_db, remplir_table_oeuvre
-from fonctions import separation_par_types
+from fonctions import separation_par_types, avoir_salles
 
 app = Flask(__name__)
 
@@ -20,3 +20,9 @@ def plan():
 def itineraire():
     ecrits, peintures, sculptures, artefacts = separation_par_types()
     return render_template("itineraire.html", peintures=peintures, ecrits=ecrits, sculptures=sculptures, artefacts=artefacts)
+
+@app.route('/resultat', methods=['POST'])
+def resultat():
+    liste_oeuvres = request.form.getlist('check')
+    liste_salles = avoir_salles(liste_oeuvres)
+    return render_template("resultat.html", liste_oeuvres=liste_oeuvres, liste_salles=liste_salles)
--- fonctions.py
+++ fonctions.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from init import recuperer_les_oeuvres
 
 def separation_par_types():
@@ -16,3 +18,18 @@ def separation_par_types():
         elif oeuvre[0] == "Artefact":
             type_artefact.append(oeuvre)
     return type_ecrit, type_peinture, type_sculpture, type_artefact
+
+def avoir_salles(liste_oeuvres):
+    liste_salles = []
+    conn = sqlite3.connect('db/database.db')
+    for oeuvre in liste_oeuvres:
+        cur = conn.cursor()
+        cur.execute("""SELECT salle FROM oeuvres WHERE nom=?""", (oeuvre,))
+        records = cur.fetchall()
+        for r in records:
+            salle = r[0].replace('_', ' ')
+            if salle not in liste_salles:
+                liste_salles.append(salle)
+        cur.close()
+    conn.close()
+    return liste_salles
